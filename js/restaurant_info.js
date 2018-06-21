b7b7c4bf0ab4d0859059ede3e2c3c282fa36d7ad
Josh Lockrow <jlockrow@gmail.com>
2018-06-20 22:43:39 -0500

    feat: Adds review-user class for css styling

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -150,6 +150,7 @@ createReviewHTML = (review) => {
   const li = document.createElement('li');
   const name = document.createElement('p');
   name.innerHTML = review.name;
+  name.className = 'restaurant-review-user';
   li.appendChild(name);
 
   const date = document.createElement('p');
