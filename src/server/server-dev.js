864a790f69a3adf01354bbcb65c3466614c6105d
jelockro <jlockrow@gmail.com>
2018-12-27 16:58:15 -0600

    chore: remove comments

--- src/server/server-dev.js
+++ src/server/server-dev.js
@@ -3,28 +3,26 @@ import express from 'express'
 import webpack from 'webpack'
 import webpackDevMiddleware from 'webpack-dev-middleware'
 import config from '../../webpack.dev.config.js'
-//const compression = require('compression');
 
-// Simple no frills Express.js server that serves files from the app folder.
 const app = express(),
-    DIST_DIR = __dirname,
-    HTML_FILE = path.join(DIST_DIR, 'index.html'),
-    compiler = webpack(config)
+            DIST_DIR = __dirname,
+            HTML_FILE = path.join(DIST_DIR, 'index.html'),
+            compiler = webpack(config)
 
 app.use(webpackDevMiddleware(compiler, {
-    publicPath: config.output.publicPath
+  publicPath: config.output.publicPath
 }))
 
 app.get('*', (req, res, next) => {
-    compiler.outputFileSystem.readFile(HTML_FILE, (err, result) => {
-    if (err) {
-      return next(err)
-    }
-    res.set('content-type', 'text/html')
-    res.send(result)
-    res.end()
-    })
+  compiler.outputFileSystem.readFile(HTML_FILE, (err, result) => {
+  if (err) {
+    return next(err)
+  }
+  res.set('content-type', 'text/html')
+  res.send(result)
+  res.end()
   })
+})
 
 const PORT = process.env.PORT || 8000
 
