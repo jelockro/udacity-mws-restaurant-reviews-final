d9c05a276952718f4ad8f68b936840f5bcca0c64
jelockro <jlockrow@gmail.com>
2018-12-28 13:34:58 -0600

    Loads the project's original index.html on dev server

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
-import './style.css';
+import './styles.css';
 import printMe from './print.js';
+import "./templates/index.html";
 
 // *** Removing service worker for faster styling development ****
 
@@ -14,26 +15,6 @@ import printMe from './print.js';
 //             });
 //     });
 // }
-
-
-
-function component() {
- 
-    var element = document.createElement('div');
-    var btn = document.createElement('button');
-
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add("hello")
-    
-    btn.innerHTML = 'Click me and check the console!';
-    btn.onclick = printMe;
-
-    element.appendChild(btn);
-
-    return element;
-  }
-  
-  document.body.appendChild(component());
 
   if (module.hot) {
       module.hot.accept('./print.js', function() {
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+//const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const webpack = require('webpack');
 const WorkboxPlugin = require('workbox-webpack-plugin');
